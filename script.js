4e6b5cb824b911b46563e5a006e172232e4e1a8f
haiderwork50080-max <haiderwork50080@gmail.com>
2026-08-01 22:00:02 +0530

    Add button click event to open WhatsApp link

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+window.open("https://wa.me/917820041833?text=Hello,%20I%20want%20to%20order%20from%20Daily%20Needs","_blank");
+});
+
+});
