a247179549064c47a655f3b996416293d2692827
haiderwork50080-max <haiderwork50080@gmail.com>
2026-08-01 22:22:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ window.open("https://wa.me/917820041833?text=Hello,%20I%20want%20to%20order%20fr
 });
 
 });
+let cart = [];
+
+function addToCart(product) {
+    cart.push(product);
+    alert(product + " added to cart!");
+}
+function viewCart() {
+    if (cart.length === 0) {
+        alert("Your cart is empty.");
+    } else {
+        alert("Items in your cart:\n\n" + cart.join("\n"));
+    }
+}
